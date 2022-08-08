45fdd4bcd467b12879663feba01f4a619d862b2d
Arkam Mazrui <superarkam@gmail.com>
2022-08-08 10:12:37 -0400

    initial

--- Option.jsx
+++
@@ -1,8 +0,0 @@
-function Option(props) {
-
-    return (
-        <div className={'option ' + (props.value ? 'checked': '')} onClick={props.toggle}>
-            <input id='val' title="select" type="checkbox" checked={props.value} onChange={(e)=>{}}/>&nbsp;{props.text}
-        </div>
-    )
-}
